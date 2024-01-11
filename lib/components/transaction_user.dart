f6ca0334725883d955177d73d577f5e8d1db85fb
Wesley Wirmond <wesleywirmond@gmail.com>
2024-01-11 13:53:55 -0300

    separate widgets in components

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'transaction_form.dart';
+import 'transaction_list.dart';
+import '../models/transaction.dart';
+
+class TransactionUser extends StatefulWidget {
+  const TransactionUser({super.key});
+
+  @override
+  State<TransactionUser> createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(id: '1', title: 'Teste 1', value: 310.76, date: DateTime.now()),
+    Transaction(id: '2', title: 'Teste 2', value: 211.30, date: DateTime.now()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransactionList(_transactions),
+        TransactionForm(),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'models/transaction.dart';
-import 'package:intl/intl.dart';
+import 'package:project_expenses/components/transaction_user.dart';
 
 main() => runApp(const ExpensesApp());
 
@@ -14,14 +13,6 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
-  final _transactions = [
-    Transaction(id: '1', title: 'Teste 1', value: 310.76, date: DateTime.now()),
-    Transaction(id: '2', title: 'Teste 2', value: 211.30, date: DateTime.now()),
-  ];
-
   MyHomePage({super.key});
 
   @override
@@ -41,91 +32,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Column(
-            children: <Widget>[
-              ..._transactions.map((tr) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ${tr.value.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            tr.title,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            DateFormat('d MMM y').format(tr.date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              }).toList()
-            ],
-          ),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    controller: titleController,
-                    decoration: InputDecoration(
-                      labelText: 'Título',
-                    ),
-                  ),
-                  TextField(
-                    controller: valueController,
-                    decoration: InputDecoration(
-                      labelText: 'Valor (R\$)',
-                    ),
-                  ),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        TextButton(
-                          child: Text(
-                            'Nova Transação',
-                            style: TextStyle(color: Colors.purple),
-                          ),
-                          onPressed: () {
-                            print(titleController.text);
-                            print(valueController.text);
-                          },
-                        )
-                      ])
-                ],
-              ),
-            ),
-          )
+          const TransactionUser()
         ],
       ),
     );
